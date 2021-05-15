0801f182ac6e3347f5b1e88c646d196cadbbf1f8
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-05-15 11:41:35 +0200

    add route for rendering with localhost

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,6 +1,9 @@
 module.exports = function(app) {
 	var usersController = require('../controllers/userController');
 		
+	app.route('/')
+		.get(usersController.show_index);
+
 	app.route('/users')
 		.get(usersController.list_users);
 };
