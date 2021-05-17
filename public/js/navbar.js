ad5e9609776aed5c83446bc9f263c902286f8434
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-05-17 19:11:59 +0200

    New navbar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,13 +1,25 @@
-var MyNavbar = {
+const NavButton = {
+  props: ["text", "route"],
+  template: 
+  `
+  <li class="nav-item">
+    <router-link class="nav-link" :to="{name:route}">{{text}}</router-link>
+  </li>
+  `
+}
+
+const Navbar = {
+  components: {
+    'navbutton': NavButton
+  },
   template: `
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div id="navbarNav">
-  <ul class="navbar-nav">
-  <navbutton text="Home"></navbutton>
-  <navbutton text="Movies"></navbutton>
-  </ul>
-  <p>Tot click: {{count}}</p>
-  </div>
+    <div id="navbarNav">
+      <ul class="navbar-nav">
+        <navbutton text="Home" route="/"></navbutton>
+        <navbutton text="Users" route="/users"></navbutton>
+      </ul>
+    </div>
   </nav>
   `
 }
