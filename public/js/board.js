867c9efdcdf647b2b21bb071fd8a9d89cde6974a
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-05-19 17:41:18 +0200

    add board table without style

--- public/js/board.js
+++ public/js/board.js
@@ -1,17 +1,62 @@
-const Board = {
+const HeadersRow = {
+    props: ["topics"],
     template: 
     `
-    <div class="bg-white h-100">
-        <h1>{{ $route.params.title }}</h1>
-        <table>
-            {{board}}
+    <tr>
+        <th></th>
+        <th v-for="topic in topics" :key="topic">{{topic}}</th>
+    </tr>
+    `
+}
+
+const TasksRow = {
+    props: ["topics", "tasks"],
+    template: 
+    `
+    <tr>
+        <td>Available Tasks</td>
+        <td v-for="topic in topics" :key="topic">
+            <p v-for="task in tasks" :key="task" v-if="task.user==null && task.topic==topic">{{ task.name }}</p>
+        </td>
+    </tr>
+    `
+}
+
+const Row = {
+    props: ["member", "topics", "tasks"],
+    template: 
+    `
+    <tr>
+        <td>{{member}}</td>
+        <td v-for="topic in topics" :key="topic">
+            <p v-for="task in tasks" :key="task" v-if="task.user==member && task.topic==topic">{{ task.name }}</p>
+        </td>
+    </tr>
+    `
+}
+
+const Board = {
+    components: {
+        'headers': HeadersRow,
+        'tasks': TasksRow,
+        'row': Row
+    },
+    template: 
+    `
+    <div class="bg-white">
+        <h1>{{ board.title }}</h1>
+        <p>{{ board.description }}</p>
+        <table style="width:100%">
+            <headers :topics="board.topics"></headers>
+            <tasks :tasks="board.tasks" :topics="board.topics"></tasks>
+            <row v-for="member in board.members" :key="member" :member="member" :tasks="board.tasks" :topics="board.topics"></row>
         </table>
     </div>
     `,
     data: function() {
         return {
             params: null,
-            board: null
+            board: {}
         }
     },
     methods: {
@@ -22,7 +67,7 @@ const Board = {
         mountTable() {
             axios.get("http://localhost:3000/api/board/"+this.params.owner+"/"+this.params.title)
             .then(response => {
-                this.board = response.data;
+                this.board = response.data[0];
             });
         }
     },
