416290d3afaf9c471b80138e6907aec4e81b949e
Tommaso Mandoloni <tommaso.mandoloni@studio.unibo.it>
2021-05-20 20:14:35 +0200

    homepage refactor

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -21,7 +21,7 @@ const Dashboard = {
     <div>
         <h1 class="mt-3">Hi, {{session_user.username}} !</h1>
         <h3 class="mt-5">Your projects</h3>
-        <div class="row">
+        <div class="row d-flex flex-wrap">
             <div class="col p-0 mt-3">
                 <div class="card-body p-0">
                     <div class="row no-gutters">
--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -12,44 +12,19 @@ const Sidebar = {
         'sidebutton': Sidebutton
     },
     template: `
-        <div class="row">
-            <div class="col mx-3 px-3">
-                <ul class="nav flex-column bg-white p-2 border border-primary">
-                    <sidebutton text="Dashboard" route="/"></sidebutton>
-                    <sidebutton text="New Project" route="/project"></sidebutton>
-                    <hr/>
-                    <sidebutton text="About" route="/about"></sidebutton>
-                    <sidebutton text="Settings" route="/settings"></sidebutton>
-                    <li class="nav-item border border-primary">
-                        <a class="nav-link" href="/logout">Logout</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+      <aside class="col-2 vh-100">
+        <ul class="nav d-flex flex-column bg-white p-2 border border-primary h-75">
+          <sidebutton text="Dashboard" route="/"></sidebutton>
+          <sidebutton text="New Project" route="/project"></sidebutton>
+          <hr/>
+          <hr/>
+          <hr/>
+          <sidebutton text="About" route="/about"></sidebutton>
+          <sidebutton text="Settings" route="/settings"></sidebutton>
+          <li class="nav-item border border-primary">
+            <a class="nav-link" href="/logout">Logout</a>
+          </li>
+        </ul>
+      </aside>
     `
 }
-/*
-* ,
-    data: function() {
-        return {
-            session_user: {}
-        }
-    },
-    methods: {
-        init() {
-            this.getSessionUser();
-        },
-        getSessionUser() {
-            axios.get("http://localhost:3000/session/user")
-            .then( response => {
-                this.session_user = response.data;
-            })
-        },
-        replaceByDefault(e){
-            e.target.src = "https://icon-library.net//images/not-found-icon/not-found-icon-4.jpg"
-        }
-    },
-    mounted: function(){
-        this.init();
-    }
-* */
