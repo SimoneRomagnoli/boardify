fff581a1d762552037f2b6f9083f808547a52cca
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-05-22 15:51:33 +0200

    refactor back btn

--- public/js/task.js
+++ public/js/task.js
@@ -1,14 +1,16 @@
 const Task = {
     template: `
-    <div>
+    <div class="card-body">
+        <div class="card col-sm-1 text-center">
+            <router-link class="nav-link rounded" :to="'/board/'+params.owner+'/'+params.title">
+                <i class="fas fa-arrow-left"></i>
+            </router-link>
+        </div>
         <h3 class="mt-5">Task {{task.name}} // {{task.state}}</h3>
         <p>Topic: {{task.topic}}</p>
         <p>Description: {{task.description}}</p>
         <p>User: {{task.user}}</p>
         <p>Comment: {{task.comment}}</p>
-        <router-link class="nav-link rounded" :to="'/board/'+params.owner+'/'+params.title">
-            <i class="fas fa-arrow-left"></i>
-        </router-link>
     </div>
     `,
     data: function() {
