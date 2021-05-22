493cab8ff99153e301c3c7369d981376c147510a
Tommaso Mandoloni <tommaso.mandoloni@studio.unibo.it>
2021-05-22 23:10:56 +0200

    working modal

--- public/js/board.js
+++ public/js/board.js
@@ -9,6 +9,53 @@ const HeadersRow = {
     `
 }
 
+const TaskModal = {
+    props: ["args"],
+    template:
+    `
+      <div class="modal fade" id="taskModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="modalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                ...
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+      </div>
+    `,
+    data: function () {
+        return {
+            task: null,
+            params: this.args
+        }
+    },
+    methods: {
+        init() {
+            //this.mountTask();
+        },
+        mountTask() {
+            axios.get("http://localhost:3000/api/board/"+this.params.owner+"/"+this.params.title+"/"+this.params.task)
+                .then(response => {
+                    this.task = response.data[0];
+                });
+        }
+    },
+    mounted: function () {
+        this.init();
+    }
+
+}
+
 const TasksRow = {
     props: ["topics", "tasks", "args"],
     template: 
@@ -16,7 +63,7 @@ const TasksRow = {
     <tr>
         <td class="text-center font-weight-bold">Available Tasks</td>
         <td v-for="topic in topics" :key="topic" class="p-2">
-            <a href="" class="border rounded border-dark bg-danger text-capitalize text-white m-2" v-for="task in tasks" :key="task" v-if="task.user===null && task.topic===topic" @click.prevent="assignTask(task)">{{ task.name }}</a>
+          <a href="" class="border rounded border-dark bg-danger text-capitalize text-white m-2" v-for="task in tasks" :key="task" v-if="task.user===null && task.topic===topic">{{ task.name }}</a>
         </td>
     </tr>
     `
@@ -39,6 +86,7 @@ const TasksRow = {
                 location.replace("http://localhost:3000/"); // load home page but need to reload page
             });
         }
+        //@click.prevent="assignTask(task)"
     },
     mounted: function() {
         this.init();
@@ -49,15 +97,12 @@ const Row = {
     props: ["member", "topics", "tasks", "args"],
     template: 
     `
-    <tr>
-        <td class="text-center font-weight-bold" style="vertical-align: middle">{{member}}</td>
-        <td v-for="topic in topics" :key="topic">
-          <p class="border rounded border-dark bg-danger text-capitalize text-center text-white" v-for="task in tasks" :key="task" v-if="task.user===member && task.topic===topic">
-            <router-link class="nav-link text-light rounded" :to="'/board'+'/'+args.owner+'/'+args.title+'/'+task.name">{{task.name}}</router-link>
-            <a href="#" class="badge float-right mr-2 text-center text-secondary" @click.prevent="removeTask(task)">X</a>
-          </p>
-        </td>
-    </tr>
+      <tr>
+          <td class="text-center font-weight-bold" style="vertical-align: middle">{{member}}</td>
+          <td v-for="topic in topics" :key="topic">
+            <button type="button" class="btn border rounded border-dark bg-danger text-capitalize text-center text-white" data-toggle="modal" data-target="#taskModal" v-for="task in tasks" :key="task" v-if="task.user===member && task.topic===topic">{{ task.name }}</button>
+          </td>
+      </tr>
     `,
     data: function() {
         return {
@@ -82,13 +127,34 @@ const Board = {
     components: {
         'headers': HeadersRow,
         'tasks': TasksRow,
-        'row': Row
+        'row': Row,
+        'modal': TaskModal
     },
     template: 
     `
     <div class="bg-white p-3">
         <h1>{{ board.title }}</h1>
         <p>{{ board.description }}</p>
+        <!--<modal :args="args"></modal>-->
+        <div class="modal fade" id="taskModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="modalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                ...
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
         <table class="table table-bordered">
             <headers :topics="board.topics"></headers>
             <tasks :tasks="board.tasks" :topics="board.topics" :args="params"></tasks>
@@ -98,13 +164,13 @@ const Board = {
     `,
     data: function() {
         return {
-            params: null,
-            board: {}
+            params: this.$route.params,
+            board: {},
+            showModal: false
         }
     },
     methods: {
         init() {
-            this.params = this.$route.params;
             this.mountTable();
         },
         mountTable() {
