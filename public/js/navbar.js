81bd4f928f8742d082ace633e4cb1ebac4ae0f1b
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-06-01 13:45:18 +0200

    navbar with beaver logo and better button distribution

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,11 +1,10 @@
 const NavButton = {
-  props: ["text", "route", "icon"],
+  props: ["route", "icon"],
   template: 
   `
-  <li class="nav-item rounded d-inline-flex mr-4">
-    <router-link class="nav-link text-white" :to="route">
-      <i :class="icon" class="text-white align-self-center pr-1"></i>
-      {{text}}
+  <li class="nav-item rounded d-inline-flex p-0">
+    <router-link class="nav-link" :to="route">
+      <i :class="icon" class="text-white bfy-navbar-icon align-self-center p-0"></i>
     </router-link>
   </li>
   `
@@ -23,34 +22,39 @@ const Navbar = {
         <i class="fas fa-bars fa-1x"></i>
       </span>
     </button>
-    <div class="collapse navbar-collapse" id="navbar">
-      <ul class="navbar-nav">
-        <navbutton text="Home" route="/" icon="fa fa-tachometer"></navbutton>
-        <navbutton text="Profile" :route="profile_path" icon="fa fa-user"></navbutton>
-        <navbutton text="New Project" route="/project" icon="fa fa-plus-square"></navbutton>
-        <navbutton text="Notifications" route="/notifications" icon="fa fa-bell"></navbutton>
-      </ul>
+    <div class="collapse navbar-collapse nav-item navbar-nav mx-3" id="navbar">
+      <router-link to="/">
+        <img class="logo" style="max-height: 50px;" src="/static/img/boardify-beaver-logo-big.png" alt="Boardify"></img>
+      </router-link>
     </div>
     
-    <img class="brand" style="max-height: 45px;" src="/static/img/boardify-logo.png" alt="Boardify">
-    
-    <div class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="fa fa-lg fa-user-circle text-white align-self-center"></i>
-      </a>
-      <div class="dropdown-menu dropdown-menu-right mt-2" aria-labelledby="navbarDropdownMenuLink">
-        <router-link class="dropdown-item" to="/about">
-          <i class="fa fa-info-circle align-self-center pr-1"></i>
-          About
-        </router-link>
-        <router-link class="dropdown-item" to="/settings">
-          <i class="fa fa-gear align-self-center pr-1"></i>
-          Settings
-        </router-link>
-        <a class="dropdown-item" href="/logout">
-          <i class="fa fa-sign-out align-self-center pr-1"></i>
-          Logout
+    <div class="nav-item pull-right row">
+      <ul class="navbar-nav">
+        <navbutton route="/notifications" icon="fa fa-bell"></navbutton>
+        <navbutton route="/project" icon="fa fa-plus-square"></navbutton>
+      </ul>
+      <div class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-lg fa-user-circle text-white align-self-center bfy-navbar-icon"></i>
         </a>
+        <div class="dropdown-menu dropdown-menu-right mt-2" aria-labelledby="navbarDropdownMenuLink">
+          <router-link class="dropdown-item" :to="profile_path">
+            <i class="fa fa-user align-self-center pr-1"></i>
+            Profile
+          </router-link>
+          <router-link class="dropdown-item" to="/about">
+            <i class="fa fa-info-circle align-self-center pr-1"></i>
+            About
+          </router-link>
+          <router-link class="dropdown-item" to="/settings">
+            <i class="fa fa-gear align-self-center pr-1"></i>
+            Settings
+          </router-link>
+          <a class="dropdown-item" href="/logout">
+            <i class="fa fa-sign-out align-self-center pr-1"></i>
+            Logout
+          </a>
+        </div>
       </div>
     </div>
     
