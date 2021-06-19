d943bd4b9e122c6c299d7550faa79ecc124927c2
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-06-19 11:30:58 +0200

    navbar fixed in phone mode

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,11 +2,11 @@ const NavButton = {
   props: ["route", "icon"],
   template: 
   `
-  <li class="nav-item rounded d-inline-flex p-0">
+  <div class="nav-item rounded d-inline-flex p-0">
     <router-link class="nav-link" :to="route">
       <i :class="icon" class="text-white bfy-navbar-icon align-self-center p-0"></i>
     </router-link>
-  </li>
+  </div>
   `
 }
 
@@ -16,23 +16,17 @@ const Navbar = {
   },
   template: `
   <nav class="navbar navbar-expand-lg navbar-light bfy-bg-navbar">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar"
-          aria-controls="navbar" aria-expanded="false" aria-label="toggle navigation">
-      <span class="text-white">
-        <i class="fas fa-bars fa-1x"></i>
-      </span>
-    </button>
-    <div class="collapse navbar-collapse nav-item navbar-nav mx-3" id="navbar">
+    <div class="nav-item navbar-nav mx-3">
       <router-link to="/">
         <img class="logo" style="max-height: 50px;" src="/static/img/boardify-beaver-logo-big.png" alt="Boardify"></img>
       </router-link>
     </div>
     
-    <div class="nav-item pull-right row">
-      <ul class="navbar-nav">
-        <navbutton class="mr-3" route="/notifications" icon="fa fa-bell"></navbutton>
-        <navbutton class="mr-3" route="/project" icon="fa fa-plus-square"></navbutton>
-      </ul>
+    <div class="nav-item ml-auto row">
+      
+        <navbutton  route="/notifications" icon="fa fa-bell"></navbutton>
+        <navbutton  route="/project" icon="fa fa-plus-square"></navbutton>
+      
       <div class="nav-item dropdown">
         <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i class="fa fa-lg fa-user-circle text-white align-self-center bfy-navbar-icon"></i>
