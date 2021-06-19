187a8346cfb116d8f5c9293c3f6b9c7ad9627133
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-06-19 14:24:29 +0200

    notification page with better layout

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -1,18 +1,27 @@
 const NotificationSummary = {
-    props: ["project", "message", "read", "url"],
+    props: ["project", "message", "read", "url", "object", "date"],
     template: `
-    <div class="row rounded-lg bfy-bg-table-cell m-2 p-2">
-        <div class="col-sm-2 p-2">
+    <div class="row rounded-lg bfy-bg-table-cell m-2 p-2 d-flex">
+        <div class="align-self-center">
+            <span v-if="!read" class="fa fa-circle bfy-notification-color ml-2"></span>
+            <span v-else class="fa fa-circle-thin bfy-notification-color ml-2"></span>
+        </div>
+        <div class="col-sm-4 p-2">
             <strong class="col-sm-2 p-2">{{project.owner}} - {{project.title}} </strong>
         </div>    
-        <div class="col-sm-6 p-2">
-            {{message}}
+        <div class="col-sm-4 p-2">
+            {{message}}: {{object}}
         </div>
-        <div class="col p-2">
-            
+        <div class="p-2 ml-auto">
+        <router-link class="bfy-bg-card-button text-white rounded p-1 text-center border-0" :to="url" style="text-decoration: none">
+            Open board
+        </router-link>
         </div>
     </div>
-    `
+    `,
+    methods: {
+
+    }
 }
 
 
@@ -22,9 +31,13 @@ const Notifications = {
     },
     template: `
     <div class="p-3 vh-100">
-        <h1 class="px-0">Notifications</h1>
-        <div class="container-fluid bg-white shadow rounded-lg p-2">
-            <notification-summary v-for="notification in notifications" :key="notification" :project="notification.project" :message="notification.message" :read="notification.read" :url="notification.url"></notification-summary>
+        <div class="container">
+            <h1 class="px-0">Notifications</h1>
+            <div class="bg-white shadow rounded-lg p-2">
+                <notification-summary v-for="notification in notifications" :key="notification" 
+                    :project="notification.project" :message="notification.message" :object="notification.object" :read="notification.read" :url="notification.url" :date="notification.date">
+                </notification-summary>
+            </div>
         <div>
     </div>
     `,
