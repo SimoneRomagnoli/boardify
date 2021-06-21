292871ea99ea3c42c1a4e07f80f098b04dd288f7
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-06-21 14:53:13 +0200

    add card to project page

--- public/js/project.js
+++ public/js/project.js
@@ -6,73 +6,75 @@ const Project = {
           <div class="row justify-content-center pt-6">
             <div class="col-xl-4 col-lg-5 col-md-6">
               <div class="text-center mb-4">
-                <h1 class="mb-1">Create new project</h1>
+                <h1 class="mb-1">Create a new project</h1>
               </div>
-              <form method="POST">
-                <div class="form-group mb-0">
-                  <input
-                    type="text"
-                    name="title"
-                    placeholder="Title"
-                    id="title"
-                    class="form-control mb-2"
-                    v-model="board.title"
-                  />
-                  <p v-if="msg != null">{{msg}}</p>
-                </div>
-                <div class="form-group mb-0">
-                  <textarea
-                    type="text"
-                    name="description"
-                    placeholder="Description"
-                    id="description"
-                    class="form-control mb-2"
-                    v-model="board.description"
-                  ></textarea>
-                </div>
-                <div class="form-group mb-0">
-                  <div class="input-group mb-2">
+              <div class="card-body border rounded-lg bfy-bg-project">
+                <form method="POST">
+                  <div class="form-group mb-0">
                     <input
-                        type="text"
-                        name="member"
-                        placeholder="Member"
-                        id="member"
-                        class="form-control"
-                        v-model="member"
+                      type="text"
+                      name="title"
+                      placeholder="Title"
+                      id="title"
+                      class="form-control mb-2"
+                      v-model="board.title"
                     />
-                    <button class="btn btn-success input-group-btn ml-1" @click.prevent="addMember">+</button>
+                    <p v-if="msg != null">{{msg}}</p>
                   </div>
-                  <p v-if="error.present">{{error.message}}</p>
-                  <ul class="list-group">
-                    <li class="list-group-item d-flex justify-content-between align-items-center py-1 pr-0 pl-3" v-for="member in board.members" :key="member">
-                      {{member}}
-                      <a href="#" class="badge badge-danger float-right mr-2" @click.prevent="removeMember(member)">X</a>
-                    </li>
-                  </ul>
-                </div>
-                <div class="form-group mb-0 mt-2">
-                  <div class="input-group mb-2">
-                    <input
-                        type="text"
-                        name="topic"
-                        placeholder="Topic"
-                        id="topic"
-                        class="form-control"
-                        v-model="topic"
-                    />
-                    <button class="btn btn-success input-group-btn ml-1" @click.prevent="addTopic">+</button>
+                  <div class="form-group mb-0">
+                    <textarea
+                      type="text"
+                      name="description"
+                      placeholder="Description"
+                      id="description"
+                      class="form-control mb-2"
+                      v-model="board.description"
+                    ></textarea>
                   </div>
-                  <ul class="list-group">
-                    <li class="list-group-item d-flex justify-content-between align-items-center py-1 pr-0 pl-3" v-for="topic in board.topics" :key="topic">
-                      {{topic}}
-                      <a href="#" class="badge badge-danger float-right mr-2" @click.prevent="removeTopic(topic)">X</a>
-                    </li>
-                  </ul>
-                </div>
-                <button @click.prevent="create" class="btn-block btn btn-success mt-2">
-                  Create
-                </button>
-              </form>
+                  <div class="form-group mb-0">
+                    <div class="input-group mb-2">
+                      <input
+                          type="text"
+                          name="member"
+                          placeholder="Member"
+                          id="member"
+                          class="form-control"
+                          v-model="member"
+                      />
+                      <button class="btn btn-success input-group-btn ml-1" @click.prevent="addMember">+</button>
+                    </div>
+                    <p v-if="error.present">{{error.message}}</p>
+                    <ul class="list-group">
+                      <li class="list-group-item d-flex justify-content-between align-items-center py-1 pr-0 pl-3" v-for="member in board.members" :key="member">
+                        {{member}}
+                        <a href="#" class="badge badge-danger float-right mr-2" @click.prevent="removeMember(member)">X</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="form-group mb-0 mt-2">
+                    <div class="input-group mb-2">
+                      <input
+                          type="text"
+                          name="topic"
+                          placeholder="Topic"
+                          id="topic"
+                          class="form-control"
+                          v-model="topic"
+                      />
+                      <button class="btn btn-success input-group-btn ml-1" @click.prevent="addTopic">+</button>
+                    </div>
+                    <ul class="list-group">
+                      <li class="list-group-item d-flex justify-content-between align-items-center py-1 pr-0 pl-3" v-for="topic in board.topics" :key="topic">
+                        {{topic}}
+                        <a href="#" class="badge badge-danger float-right mr-2" @click.prevent="removeTopic(topic)">X</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <button @click.prevent="create" class="btn-block btn btn-success mt-2">
+                    Create
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
