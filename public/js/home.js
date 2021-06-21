c70730c34c7d5c3bce068d5439f4c6cf6180ba60
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-06-21 16:28:39 +0200

    change home cards

--- public/js/home.js
+++ public/js/home.js
@@ -2,13 +2,13 @@ const ProjectHeader = {
     props: ["title", "owner", "members", "route"],
     template: 
     `
-    <div class="card mt-3 shadow-lg rounded p-3 mr-3">
-        <h4 class="card-title my-1">{{title}}</h4>
-        <hr/>
-        <p class="card-text">Owner: {{owner}}</p>
-        <p class="card-text">Members: {{members}}</p>
-        <router-link class="nav-link w-100 bfy-bg-card-button text-white font-weight-bold rounded px-5 text-center" :to="route">Open board</router-link>
-    </div>
+    <a :href="route" class="text-dark" style="text-decoration: none">
+        <div class="card shadow-lg rounded-lg p-3 bfy-bg-hover">
+            <h5 class="card-title mb-2"><i class="fas fa-clipboard-list bfy-green mr-1"></i> {{title}}</h5>
+            <p class="card-text my-0">Owner: {{owner}}</p>
+            <span class="ml-auto">{{members}} <i class="fas fa-users bfy-green"></i> </span>
+        </div>
+    </a>
     `
 }
 
@@ -18,17 +18,21 @@ const Dashboard = {
         'project-header': ProjectHeader
     },
     template: `
-    <div>
-        <h1 class="mt-3">Welcome to Boardify, {{session_user.username}}!</h1>
-        <h3 class="mt-5">Your boards</h3>
-        <div class="row">
-            <div class="col">
-                <div class="card-body p-0">
-                    <div class="row no-gutters">
-                        <div class="col-xs-3" v-for="project in projects" :key="project._id">
-                            <project-header :title="project.title" :owner="project.owner" :members="project.members.length" :route="'/board'+'/'+project.owner+'/'+project.title"></project-header>
+    <div class="px-4">
+        <h2 class="mt-3">Welcome to Boardify, {{session_user.firstname}}!</h2>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-7 col-12 px-0">
+                    <h3 class="mt-5">Your boards</h3>
+                    <div class="card-body p-0">
+                        <div class="row">
+                            <div class="col-md-6 col-12 p-1" v-for="project in projects" :key="project._id">
+                                <project-header :title="project.title" :owner="project.owner" :members="project.members.length" :route="'/board'+'/'+project.owner+'/'+project.title"></project-header>
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div class="col-sm-4 col-12">
                 </div>
             </div>
         </div>
@@ -68,6 +72,6 @@ const Home = {
         'dashboard': Dashboard
     },
     template: `
-        <dashboard class="px-4"></dashboard>
+        <dashboard></dashboard>
     `
  }
